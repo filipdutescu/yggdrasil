250c7832591f6bb219ebdbc0514fd8bc553850aa
Filip Dutescu <filip@hucksy.dev>
2023-08-21 14:51:39 +0300

    chore: init NixOs config
    Signed-off-by: Filip Dutescu <filip@hucksy.dev>

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{
+  sources ? import ./nix/sources.nix,
+  pkgs ? import sources.nixpkgs {}
+}:
+let
+  name = "Yggdrasil";
+in
+  pkgs.mkShell {
+    inherit name;
+
+    buildInputs = with pkgs; [
+      # development packages
+      git
+
+      # general Unix utils
+      curl
+      jq
+      which
+
+      # others
+      pfetch
+    ];
+  }
