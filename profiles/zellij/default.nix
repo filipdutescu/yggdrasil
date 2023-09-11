bf5b9ae744669c247bc856fbe29bd6e29871fc80
Filip Dutescu <filip@hucksy.dev>
2023-09-11 22:09:52 +0300

    feat(zsh): add vim bindings
    Signed-off-by: Filip Dutescu <filip@hucksy.dev>

--- profiles/zellij/default.nix
+++ profiles/zellij/default.nix
@@ -3,7 +3,6 @@
     enable = true;
 
     enableZshIntegration = true;
-    defaultKeymap = "vicmd";
 
     settings = {
       keybinds = {
--- profiles/zsh/default.nix
+++ profiles/zsh/default.nix
@@ -2,6 +2,8 @@
   programs.zsh = {
     enable = true;
 
+    defaultKeymap = "vicmd";
+
     shellAliases = {
       dt = "delta";
       z = "zellij";
