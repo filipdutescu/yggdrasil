41c3f63ea017f480eea2536ba5f0c4847ebecde0
Filip Dutescu <filip@hucksy.dev>
2023-09-12 12:46:01 +0300

    refactor(starship): split into modules
    Signed-off-by: Filip Dutescu <filip@hucksy.dev>

--- profiles/starship/default.nix
+++ profiles/starship/default.nix
@@ -1,5 +1,7 @@
 let
-  concatStrings = stringsList: builtins.concatStringsSep "" stringsList;
+  modules = import ./modules.nix;
+  palette = import ./palette.nix;
+  prompt = import ./prompt.nix;
 in {
   programs.starship = {
     enable = true;
@@ -12,120 +14,9 @@ in {
       scan_timeout = 10;
 
       username.format = "[$user]($style) ";
-
-      format = concatStrings [
-        "$username"
-        "$hostname"
-        "$nix_shell"
-        "$localip"
-        "$kubernetes"
-        "$c"
-        "$cmake"
-        "$cobol"
-        "$daml"
-        "$dart"
-        "$deno"
-        "$dotnet"
-        "$elixir"
-        "$elm"
-        "$erlang"
-        "$fennel"
-        "$golang"
-        "$guix_shell"
-        "$haskell"
-        "$haxe"
-        "$helm"
-        "$java"
-        "$julia"
-        "$kotlin"
-        "$gradle"
-        "$lua"
-        "$nim"
-        "$nodejs"
-        "$ocaml"
-        "$opa"
-        "$perl"
-        "$php"
-        "$pulumi"
-        "$purescript"
-        "$python"
-        "$raku"
-        "$rlang"
-        "$red"
-        "$ruby"
-        "$rust"
-        "$scala"
-        "$solidity"
-        "$swift"
-        "$terraform"
-        "$vlang"
-        "$vagrant"
-        "$zig"
-        "$crystal"
-        "$cmd_duration"
-        "$directory"
-        "$character"
-      ];
-      right_format = concatStrings [
-        "$git_branch"
-        "$git_commit"
-        "$git_state"
-        "$git_metrics"
-        "$git_status"
-        "$docker_context"
-      ];
-
-      aws.disabled = true;
-      azure.disabled = true;
-      battery.disabled = true;
-      buf.disabled = true;
-      bun.disabled = true;
-      c.disabled = true;
-      cmake.disabled = true;
-      cobol.disabled = true;
-      conda.disabled = true;
-      crystal.disabled = true;
-      daml.disabled = true;
-      dart.disabled = true;
-      deno.disabled = true;
-      dotnet.disabled = true;
-      elixir.disabled = true;
-      elm.disabled = true;
-      erlang.disabled = true;
-      fennel.disabled = true;
-      gcloud.disabled = true;
-      guix_shell.disabled = true;
-      gradle.disabled = true;
-      haskell.disabled = true;
-      haxe.disabled = true;
-      java.disabled = true;
-      jobs.disabled = true;
-      julia.disabled = true;
-      kotlin.disabled = true;
-      line_break.disabled = true;
-      meson.disabled = true;
-      nim.disabled = true;
-      ocaml.disabled = true;
-      opa.disabled = true;
-      openstack.disabled = true;
-      package.disabled = true;
-      perl.disabled = true;
-      php.disabled = true;
-      pulumi.disabled = true;
-      purescript.disabled = true;
-      rlang.disabled = true;
-      raku.disabled = true;
-      red.disabled = true;
-      ruby.disabled = true;
-      scala.disabled = true;
-      singularity.disabled = true;
-      solidity.disabled = true;
-      spack.disabled = true;
-      swift.disabled = true;
-      vagrant.disabled = true;
-      vlang.disabled = true;
-      vcsh.disabled = true;
-      zig.disabled = true;
-    };
+    }
+    // modules
+    // palette
+    // prompt;
   };
 }
---
+++ profiles/starship/palette.nix
@@ -0,0 +1,26 @@
+{
+  palette = "springan";
+
+  palettes = {
+    springan = {
+      black = "#1e1f21";
+      red = "#c16a6d";
+      green = "#8ab97b";
+      blue = "#7b82b9";
+      yellow = "#c9c76b";
+      purple = "#b17aba";
+      cyan = "#7bb0b9";
+      white = "#d3d0cb";
+
+      # light colours
+      light-black = "#303436";
+      light-red = "#c97e80";
+      light-green = "#a1c794";
+      light-blue = "#949ac7";
+      light-yellow = "#d3d175";
+      light-purple = "#c193c8";
+      light-cyan = "#94bfc7";
+      light-white = "#e7e5df";
+    };
+  };
+}
